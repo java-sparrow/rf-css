c8ed1062fcd4b85bff13a554276a29d8928b0df2
treelite <c.xinle@gmail.com>
2013-07-18 03:03:28 +0800

    add build config

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        name: 'rf',
+        clean : ['dist/*', '<%= name %>.zip'],
+        less: {
+            release: {
+                options: {
+                    paths: ['src']
+                },
+                files: {
+                    'dist/<%= name %>.css': 'src/<%= name %>.less'
+                }
+            }
+        },
+        copy: {
+            // TODO: 过滤'-source'文件
+            img: {
+                files: [
+                    {expand: true, cwd: 'src/img', src: '*', dest: 'dist/img/'}
+                ]
+            }
+        },
+        zip: {
+            main: {
+                router: function (filepath) {
+                    return 'rf-css/' + filepath;
+                },
+
+                src: [
+                    'package.json', 
+                    'README.md', 
+                    'dist/**'
+                ],
+
+                dest: '<%= name %>.zip'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-zip');
+
+    grunt.registerTask('default', ['clean', 'less:release', 'copy:img']);
+    grunt.registerTask('release', ['default', 'zip']);
+}
